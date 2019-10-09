276ec93d8d0e690b0efc7555d02e860c686886cc
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2019-10-09 20:52:36 +0700

    Add files via upload

---
+++ src/if1/pkg10118026/latihan18/kelipatan/IF110118026Latihan18Kelipatan.java
@@ -0,0 +1,25 @@
+
+package if1.pkg10118026.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA			: Riski Dwi Sabariyanto
+ * KELAS		: IF-1
+ * NIM			: 10118026
+ * Deskripsi Program	: Menampilkan bilangan kelipatan
+ */
+public class IF110118026Latihan18Kelipatan {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		double a = 3.5, b = 0;
+		for (int i = 1; i <= 10; i++, b = a * i)
+			System.out.println(b);
+
+	}
+	
+}
